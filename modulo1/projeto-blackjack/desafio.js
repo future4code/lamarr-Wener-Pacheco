63daa0b33e795cd6c9917caf47c1f89386be7c7d
Wener Simonassi Borges Pacheco <wenersimonassi@gmail.com>
2022-05-07 20:25:18 -0300

    tentativa-resolucao-desafio

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,53 +11,138 @@
  * 
  */
 
+
+
+
+
+
     console.log('Boas vindas ao jogo de BlackJack')
-
-    let novaRodada=((confirm('Quer iniciar uma nova rodada?'))===true)
- 
-   switch (novaRodada){
-   case true :
-   let carta1 = comprarCarta()
-   let carta2 = comprarCarta()
-   let carta3 = comprarCarta()
-   let carta4 = comprarCarta()
-
-         if (carta1===carta2==="A"){
-         carta1= (carta5 = comprarCarta())
-         carta2= (carta6 = comprarCarta())
-
-      }else if(carta3===carta4==="A"){
-      carta3= (carta7 = comprarCarta())
-      carta4= (carta8 = comprarCarta())
-   }else{
-      carta1=carta1
-      carta2=carta2
-      carta3=carta3
-      carta4=carta4
-   }
-
-   let valorCartaUsuario = carta1.valor + carta2.valor
-   let valorCartaComputador = carta3.valor + carta4.valor
-
-   let revelaCarta= ((confirm(`Suas cartas são ${carta1.texto} e ${carta2.texto} . A carta revelada do computador é ${carta3.texto}.\
-   Deseja comprar mais uma carta?`))===true)
-// falta item 11 e 12 do desafio
-            const resultadoJogo = (valorCartaUsuario - valorCartaComputador)
-
-      if(resultadoJogo > 0){
-         console.log('O usuário venceu!')
+   
+   const novaRodada=((confirm('Quer iniciar uma nova rodada?'))===true)
+    
+switch (novaRodada){
+   case true:
+      let carta1 = comprarCarta()
+      let carta2 = comprarCarta()
+      let carta3 = comprarCarta()
+      let carta4 = comprarCarta()
       
-      }else if(resultadoJogo < 0){
-         console.log('O computador venceu!')
-      
-      }else{
-         console.log('Empate!')
+      while (carta1.texto===carta2.texto==='A') {
+         carta1 = comprarCarta()
+         carta2 = comprarCarta()
       }
-         break;
+      while(carta3.texto===carta4.texto==='A'){
+         carta3 = comprarCarta()
+         carta4 = comprarCarta() 
+      }
+   
+//carta1= carta1.texto, carta2.valor;
+//carta2= carta2.texto, carta2.valor;
+//carta3= carta3.texto, carta3.valor;
+//carta4= carta4.texto,carta4.valor;
 
-   default:
-      console.log('O jogo acabou!')
-   break;
-}
+  let valorCartaUsuario = [carta1.valor, carta2.valor]
+  let valorCartaComputador = carta3.valor + carta4.valor
+  let textosCartas=[carta1.texto, carta2.texto]
+
+  function somaCartas(){
+   let soma = 0
+   for (var i = 0; i < valorCartaUsuario.length; i++) {
+           soma += valorCartaUsuario[i];  
+          return soma    
+   }
+ }
+
+ let valor = somaCartas(valorCartaUsuario)
+       
+ if(valor <21 ) {
+         let compraCarta=((confirm(`Suas cartas são ${textosCartas}. A carta revelada do computador é ${carta3.texto}\n Deseja comprar outra carta?`))===true)
+         
+            switch(compraCarta){   
+               case true:
+                carta = comprarCarta()
+               let textoCartasAtualizadas = textosCartas.push(carta.texto)
+               let valoresAtualizados= valorCartaUsuario.push(carta.valor)
+   let compraCartaNova=((confirm(`Suas cartas são ${textoCartasAtualizadas}. A carta revelada do computador é ${carta3.texto}\n Deseja comprar outra carta?`))===true)
+            if(compraCartaNova===true){
+                  valor = somaCartas(valoresAtualizados)
+            }
+               break;
+
+            default:
+              console.log('Fim de Jogo')
+            break;
+           }
+        }
+      }
+                  
  
- 
+
+
+
+      
+
+   //   if       
+
+   //          }else if (carta1.texto===carta2.texto==='A') {
+   //             carta1 = comprarCarta()
+   //             carta2 = comprarCarta()
+   //          }else if(carta3.texto===carta4.texto==='A') {
+   //             carta3 = comprarCarta()
+   //             carta4 = comprarCarta()
+   //          }
+       
+   //    console.log (carta1.texto ,carta2 ,carta3, carta4)
+            
+
+
+     
+      
+   // 
+     
+   // let perguntaUsuario = (confirm(`Suas cartas são ${carta1.texto} e ${carta2.texto}. A carta revelada do computador é ${carta3.texto}\n Deseja comprar outra carta?`))
+   //    while (perguntaUsuario===true) {
+   //     let carta = comprarCarta()
+   //       valorCarta = carta.valor
+   //       textoCarta = carta.texto
+   //    let textosCartasNovas=[carta1.texto, carta2.texto]
+   //          textosCartasNovas.push(carta.texto)
+     
+   //    let valoresCartas= [carta1.valor, carta2.valor]
+   //          valoresCartasNovas.push(valorCarta)   
+   //          function soma () {
+   //            
+   //             }}while(soma<=21
+
+   //          }
+
+            
+
+
+
+/*
+
+
+
+          console.log ("Usuário - cartas:",'-', valorCartaUsuario)
+          console.log ("Computador - cartas:", carta3.texto,',', carta4.texto,'-', valorCartaComputador)
+       
+          const resultadoJogo = (valorCartaUsuario - valorCartaComputador)
+    
+          if(resultadoJogo > 0){
+             console.log('O usuário venceu!')
+          
+          }else if(resultadoJogo < 0){
+             console.log('O computador venceu!')
+          
+          }else{
+             console.log('Empate!')
+          }
+             break;
+    */
+   //     default:
+   //        console.log('O jogo acabou!')
+   //     break;
+   //  }
+
+
