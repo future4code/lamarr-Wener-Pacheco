930f0f2af345de3e3de31ca08066415dca6b58d6
Wener Simonassi Borges Pacheco <wenersimonassi@gmail.com>
2022-07-28 23:50:17 -0300

    mais-uma-tentativa

---
+++ modulo3/labenusers/src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+//import {ListaUserAtual} from './components/UsuariosCadastrados';
+import{NovoUsuario} from './components/NovoCadastro'
+
+function App() {
+  return (
+    <NovoUsuario/>
+  );
+}
+
+export default App;
