4662d40ef6094e6a5d4c2c0fe535c28dd8c0f17a
Wener Simonassi Borges Pacheco <wenersimonassi@gmail.com>
2022-08-02 22:16:19 -0300

    inclusao-bases

---
+++ modulo3/revisao-react/astromatch/astromatch/src/components/Header/Header.js
@@ -0,0 +1,9 @@
+
+export function Header (){
+
+<header className="App-header">
+        <img src={casal_astronauta} className="App-logo" alt="logo" />
+        <h1><i>ASTROMATCH</i></h1>
+        <img src={coracoes} className="Header-coracoes" alt="logo" />
+          </header>
+}
