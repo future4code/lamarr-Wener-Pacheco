4ba3f21bd50f4d17f17d0d6653a3b217c6ce0388
Wener Simonassi Borges Pacheco <wenersimonassi@gmail.com>
2022-08-16 23:33:58 -0300

    formulario-login

--- modulo4/projeto-labex/src/pages/Login/LoginPage.js
+++ modulo4/projeto-labex/src/pages/Login/LoginPage.js
@@ -1,8 +1,52 @@
-import React from "react"
+import React,{useState} from "react"
 
-const Login=()=>{
+export const Login=()=>{
+    //const [email,setEmail]= useState("")
+    //const[senha,setSenha]=useState("")
+    const[form,setForm] = useState({email:"", senha:""})
+
+    const mudaEmail=(event)=>{
+        setForm({email:event.target.value})
+    }
+
+    const mudaSenha = (event)=>{
+        setForm({senha:event.target.value})
+    }
+
+
+
+    const fazerLogin=()=>{
+        //event.prevent.default
+        //axios.post(url,body,headers)
+    }
+
     return(
+        <div>
     <h1>Login</h1>
+    <form onSubmit={fazerLogin}>
+        <label htmlFor="email">E-Mail:</label>
+            <input
+            name="email"
+            id="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={mudaEmail}
+            type="email"
+            required
+            />
+        <label htmlFor="senha">Senha:</label>
+            <input
+            name="senha"
+            id="senha"
+            placeholder="Senha"
+            value={senha}
+            onChange={mudaSenha}
+            type="password"
+            required
+            />
+        <button type= "submit">Login</button>
+    </form>
+    </div>
 )}
 
 export default Login;
--- modulo4/projeto-labex/src/pages/TripDetails/TripDetailsPage.js
+++ modulo4/projeto-labex/src/pages/TripDetails/TripDetailsPage.js
@@ -1,8 +1,25 @@
 import React from "react"
 
 const TripDetails=()=>{
+    <div>
     return(
     <h1>Trip Details</h1>
-)}
+
+    <form onSubmit={novaViagem}>
+        <label></label>
+    
+        id: "tripId"
+        name: "Festança Marciana",
+        planet: "Marte",
+        date: "21/12/21",
+        description: "Uma viagem bem legal, na melhor época de marte",
+        durationInDays: 228 // Duração da viagem em dias
+    
+
+
+
+</div>
+)
+}
 
 export default TripDetails;
